8bea5c03708c6543b1d0f4433645a12a76b3e84c
danx <rushn24@gmail.com>
2023-09-23 17:22:53 -0400

    Part 3: created get routes for all cupcakes and individual cupcake, post route to create new cupcake. All tests pass

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash, session
+from flask import Flask, request, render_template, redirect, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy import text
 from models import db, connect_db, Cupcake
@@ -18,3 +18,29 @@ connect_db(app)
 @app.route('/')
 def show_index():
     return '<body>hello</body>'
+
+@app.route('/api/cupcakes')
+def list_cupcakes():
+    """respond with JSON list of cupcakes"""
+
+    all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=all_cupcakes)
+
+@app.route('/api/cupcakes/<int:cid>')
+def find_cupcake(cid):
+    """return JSON for selected cupcake by id"""
+
+    cupcake = db.session.get(Cupcake, cid)
+    return jsonify(cupcake=cupcake.serialize())
+
+@app.route('/api/cupcakes', methods=["POST"])
+def create_cupcake():
+    """create new cupcake"""
+
+    new_cupcake = Cupcake(flavor=request.json["flavor"],
+                          size=request.json["size"],
+                          rating=request.json["rating"],
+                          image=request.json["image"])
+    db.session.add(new_cupcake)
+    db.session.commit()
+    return (jsonify(cupcake=new_cupcake.serialize()), 201)
